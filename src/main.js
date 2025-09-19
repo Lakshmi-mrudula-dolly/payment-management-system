97dd25a006aed99d5b0110c1ca08867f9e9a2451
mrudalalakshmi.p <mrudalalakshmi.p@TBMDVMRUDALALAK.local>
2025-09-19 23:29:26 +0530

    Tailwind setup is done

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { createApp } from "vue";
 import router from "./router";
 import store from "./store";
 import Dashboard from "./views/Dashboard.vue";
+import "./assets/main.css";
 
 createApp(Dashboard).use(store).use(router).mount("#app");
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="p-6">
     <h1 class="text-2xl font-bold mb-6">Fintech Dashboard</h1>
+      <h1 class="text-4xl font-bold text-blue-600">Hello Tailwind v4</h1>
+
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
       <!-- Total Users -->
